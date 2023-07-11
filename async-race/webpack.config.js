a228cc898edf097a69e6a488d3dca4f581fa0778
Gleb Elenev <g.elenev@gmail.com>
2023-07-11 06:09:35 +0300

    chore: basic setup for TS, webpack & eslint

---
+++ async-race/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  target: 'web',
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  devtool,
+  devServer: {
+    port: 5000,
+    open: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: 'script.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      favicon: 'src/favicon.ico',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+      },
+    ],
+  },
+};
