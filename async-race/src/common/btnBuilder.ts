9a4cbe053b2e54d75954c0b1f654cc8bba6ac2b4
Gleb Elenev <g.elenev@gmail.com>
2023-07-12 21:45:36 +0300

    feat: add common builders

---
+++ async-race/src/common/btnBuilder.ts
@@ -0,0 +1,28 @@
+import ElementBuilder from './elementBuilder';
+
+export default class BtnBuilder extends ElementBuilder {
+  constructor(className: string, text: string, isDisabled = false) {
+    super({
+      tag: 'button',
+      classNames: className,
+      text: `${text}`,
+    });
+    if (isDisabled) {
+      this.disable();
+    } else {
+      this.enable();
+    }
+  }
+
+  public addClick(callback: () => void): void {
+    this.el.addEventListener('click', callback);
+  }
+
+  public disable(): void {
+    (this.el as HTMLButtonElement).disabled = true;
+  }
+
+  public enable(): void {
+    (this.el as HTMLButtonElement).disabled = false;
+  }
+}
---
+++ async-race/src/common/elementBuilder.ts
@@ -0,0 +1,40 @@
+import { Params } from '../utils/types';
+
+export default class ElementBuilder<T extends HTMLElement = HTMLElement> {
+  public el: T;
+
+  constructor(params: Params) {
+    this.el = document.createElement(params.tag) as T;
+    this.addClass(params.classNames);
+    this.addText(params.text);
+    this.setAttribute(params.attr);
+  }
+
+  public addClass(classes: Params['classNames']): void {
+    if (classes) {
+      this.el.classList.add(...classes.split(' '));
+    }
+  }
+
+  public addText(text: Params['text']): void {
+    if (text) {
+      this.el.textContent = text;
+    }
+  }
+
+  public setAttribute(attr: Params['attr']): void {
+    if (attr) {
+      Object.entries(attr).forEach(([key, value]) => {
+        this.el.setAttribute(key, value);
+      });
+    }
+  }
+
+  public addInner(...elements: ElementBuilder[]):void {
+    this.el.append(...elements.map((el) => el.createElement()));
+  }
+
+  public createElement(): HTMLElement {
+    return this.el;
+  }
+}
